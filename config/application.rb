988c35ad58ae17eec4041ada3d5a0dad897b543e
Anton Kostin <schnibba@gmail.com>
2016-03-02 02:59:32 +0300

    serve static assets

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Transistor
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
