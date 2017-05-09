0b4c6c50785b5fbc1ebec8285c94ceda0b2870cf
Anton Kostin <schnibba@gmail.com>
2017-05-09 21:03:13 +0300

    Fix products.js by using js only on pages with certain body class.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,33 +1,18 @@
-$(document).ready(function() {
-  var update_price;
-  update_price = function() {
-    var parsed_size, size;
-    size = $('#select_size').val();
+$(document).ready(function(){
+  if($("body").hasClass("products show")){
+    var update_price = function() {
+      var parsed_size, size;
+      size = $('#select_size').val();
 
-    // Same, as above, we need to parse
-    size_array = size.split(",")[1].replace(']','');
-    parsed_size = parseFloat(size_array).toFixed(2);
+      // Same, as above, we need to parse
+      size_array = size.split(",")[1].replace(']','');
+      parsed_size = parseFloat(size_array).toFixed(2);
+      
+      return $('#priceValue').text(parsed_size);
+    }
     
-    return $('#priceValue').text(parsed_size);
-  };
-  return update_price();
-
-});
-
-jQuery(function() {
-  var update_price;
-  update_price = function() {
-    var parsed_size, size;
-    size = $('#select_size').val();
-
-    // size is of type [7, 12.0], we need to parse it first
-    // into id and value (basically we need the last value)
-    size_array = size.split(",")[1].replace(']','');
-    parsed_size = parseFloat(size_array).toFixed(2);
-    console.log(size_array);
-    return $('#priceValue').text(parsed_size);
-  };
-  return $('#select_size').change(function(event) {
-    return update_price();
-  });
+    $('#select_size').change(function(event) {
+      update_price();
+    });
+  }  
 });
